ed0ad293ee7dffbedd5eefd1c7dba2d40af568e3
Wang Chengming <wangchengmingbbs@126.com>
2017-11-12 22:58:39 +0800

    add feat_selection

---
+++ State_Space/feat_selection.py
@@ -0,0 +1,67 @@
+import base
+import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score
+
+
+IR_rank = base.load_ir_csv()
+quote = base.load_quote_csv()
+industry = base.load_industry_csv()
+records = base.load_records_csv()
+industry_quote = base.load_industryquote_csv()
+
+
+def search_industies(port):
+    secucodes = records[records.PortCode == port].SecuCode.unique()
+    dicts = industry.set_index(['SecuCode']).to_dict()['FirstIndustryName']
+    industries = pd.Series(secucodes).map(dicts).unique()
+    return pd.Series(industries).dropna().values
+
+
+def generate_table():
+    ind = IR_rank.iloc[-300:].index.values
+    records_sub = records[records.PortCode.isin(ind)].copy()
+    dicts = industry.set_index(['SecuCode']).to_dict()['FirstIndustryName']
+    records_sub.SecuCode = records_sub.SecuCode.map(dicts)
+    records_sub.drop('StockName', 1, inplace=True)
+    records_sub.columns = ['PortCode', 'Updated', 'Industry', 'PrevWeight', 'TargetWeight']
+    records_sub.Updated = records_sub.Updated.apply(lambda x: str(x)[:10])
+    records_sub.dropna(inplace=True)
+    records_sub.index = records_sub.PortCode
+    records_sub.drop(['PortCode', 'Updated', 'PrevWeight', 'TargetWeight'], 1, inplace=True)
+    Port_Region = pd.get_dummies(records_sub, prefix='', prefix_sep='')
+    Port_Region = Port_Region.groupby('PortCode').agg(lambda x: np.sign(x.sum()))
+    return Port_Region
+
+
+def find_similar_ports(port, thresh=0.5):
+    Port_Region = generate_table()
+    dicts = industry.set_index(['SecuCode']).to_dict()['FirstIndustryName']
+    records_copy = records[records.PortCode==port].copy()
+    records_copy.StockName = records_copy.SecuCode.map(dicts)
+    industries = pd.Series(records_copy.StockName.unique()).dropna().values
+    test = Port_Region.columns.isin(industries).astype('int')
+    scores = []
+    for i in range(len(Port_Region)):
+        scores.append(f1_score(Port_Region.iloc[i, :].values, test))
+    scores = np.sort(scores)
+    ports_li = Port_Region.index[np.where(scores>=thresh)[0]].values
+    ports_li = [Port for Port in ports_li if Port != port]
+    return ports_li[-5:]
+
+
+def generate_states(industry_name, feature):
+    
+    def ave(name, day1, day2):
+        roll_day1 = industry_quote.loc[:, name].rolling(day1).mean()
+        roll_day2 = industry_quote.loc[:, name].rolling(day2).mean()
+        roll_diff = roll_day2 - roll_day1
+        tradingday = industry_quote.TradingDay.values
+        roll = pd.DataFrame(roll_diff)
+        roll.index = tradingday
+        return roll[day2-1:]
+    
+    if feature.__class__ is list:
+        return ave(industry_name, feature[0], feature[1])
+    else:
+        return None
--- base.py
+++ base.py
@@ -5,12 +5,14 @@ TIME_SPACE=50
 HISTORY=ALL_HISTORY-TIME_SPACE-1
 STOCK_NUM=1
 
-PROJ_PATH='~/Projects/PortfolioManagement'
+PROJ_PATH='/Users/wangchengming/Documents/5001Project/Snowball/RL'
 
 QUTOE_PATH=PROJ_PATH+"/data/quote.csv"
 RECORDS_PATH=PROJ_PATH+"/data/records.csv"
 NAV_PATH=PROJ_PATH+"/data/nav.csv"
 INDUTRY_PATH=PROJ_PATH+'/data/industry.csv'
+INDUSTRYQUOTE_PATH=PROJ_PATH+'/data/industry_quote.csv'
+IR_PATH=PROJ_PATH+'/data/IR_rank.csv'
 
 S1_PATH=PROJ_PATH+'/stock_avg.csv'
 MODEL1_PATH=PROJ_PATH+"/store/model1.ckpt"
@@ -37,6 +39,19 @@ def load_nav_csv():
 def load_industry_csv():
     print('load nav data....')
     df = pd.read_csv(INDUTRY_PATH)
+    return df
+
+def load_industryquote_csv():
+    print('load industry quote data...')
+    df = pd.read_csv(INDUSTRYQUOTE_PATH, encoding='gbk')
+    ind = df.loc[:0, :].values[:, 1:].tolist()[0]
+    ind = [i[:-4] for i in ind]
+    df.columns = ['TradingDay'] + ind
+    return df
+
+def load_ir_csv():
+    print('load information ratio rank data...')
+    df = pd.read_csv(IR_PATH, index_col='PortCode')
     return df
 
 def load_states_csv():
