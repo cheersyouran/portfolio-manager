bc7642aa6ef05364d2f190e3af7fcddca5848f35
“Youran” <cheersyouran@yeah.net>
2017-11-19 16:31:13 +0800

    update

--- task2/QLearningModel_beta/run.py
+++ task2/QLearningModel_beta/run.py
@@ -1,20 +1,20 @@
 import sys
+import threading
 sys.path.append('/Users/Youran/Projects/PortfolioManagement')
 from task2.QLearningModel_beta.env import Env
 from task2.QLearningModel_beta.QLearningModel import QLearningModel
 from task2.QLearningModel_beta.market import Market
 
-train_window = 50
-test_window = 10
+train_window = 150
+test_window = 30
 
 market = Market()
 
-def run_a_kind_of_model(kind):
+def run_a_kind_of_model(kind, features):
     print('#######################################################################')
     print('Kind:', kind)
     while True:
-        env = Env(train_window, test_window, market)
-        env.kind = kind
+        env = Env(train_window, test_window, market, kind, features)
         RL = QLearningModel(actions=list(range(env.nb_portcodes + 1)))
         count = 0
         episode = 1
@@ -28,15 +28,12 @@ def run_a_kind_of_model(kind):
                 RL.learn(str(obs), a, r, str(obs_))
                 rewards = rewards + r
                 obs = obs_
-                print(count)
                 count = count + 1
                 if done:
                     break
             ratio = rewards / (train_window * 10)
             print('Episode: ', episode, 'Rewards: ', rewards, 'Accuracy: ', ratio)
-            if (ratio > 0.9):
-                break
-            if (episode > 10):
+            if (ratio > 0.9) | (episode >= 10):
                 break
             episode = episode + 1
 
@@ -48,13 +45,24 @@ def run_a_kind_of_model(kind):
             rewards = rewards + r
             print('Action: ', a, 'Reward: ', r)
             obs = obs_
+            # fd = open('result.csv', 'a')
+            # for i in range(env.nb_portcodes) :
+            #     if (a == i):
+            #         fd.write(''.join([str(market.current_date),',', env.portcodes[i],',','1', '\n']))
+            #     else:
+            #         fd.write(''.join([str(market.current_date),',',env.portcodes[i],',','0', '\n']))
+            # fd.close()
             if done:
                 break
         ratio = rewards / (test_window * 10)
         print('Rewards: ', rewards, 'Accuracy: ', ratio)
         market.pass_a_day()
 
-
 # run_a_kind_of_model('银行')
-run_a_kind_of_model('计算机')
+# t1 = run_a_kind_of_model('计算机', [[1, 2], [1, 3], [1, 4], [1, 5]])
+# t2 = run_a_kind_of_model('计算机', [[1, 2]])
+# t3 = run_a_kind_of_model('计算机', ['MACD', 'BOLL', [1,3]])
+# t4 = run_a_kind_of_model('银行', [[1, 2], [1, 3], [1, 4], [1, 5]])
 
+
+added_thread = threading.Thread(target=run_a_kind_of_model('计算机', [[1, 3]]), name='T1')
