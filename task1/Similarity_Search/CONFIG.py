888595e8beec1ff0da776ef6070f72d586c8ddbd
Wang Chengming <wangchengmingbbs@126.com>
2017-12-04 10:03:10 +0800

    modify LIMITED

--- task1/Similarity_Search/CONFIG.py
+++ task1/Similarity_Search/CONFIG.py
@@ -2,7 +2,7 @@
 # Test data size.
 # If LIMITED is set False, then it will keep testing until the last day.
 # If True, then it will test for the next NUM_DAY.
-LIMITED = True
+LIMITED = False
 NUM_DAY = 3
 
 # Number of secucodes to choose from.
